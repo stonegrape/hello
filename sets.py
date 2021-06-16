ebe4945f318493ec7751938fe2ccc43a3bb400bf
stonegrape <1622313352@qq.com>
2021-06-16 09:38:44 +0800

    Python set set.add s.remove

---
+++ sets.py
@@ -0,0 +1,17 @@
+# create an empty set
+s = set()
+
+# add elements to the sets
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+print(s)
+
+s.remove(2)
+print(s)
+
+# print out how many elements are in the set
+print(f"there are {len(s)} elements in the set")
